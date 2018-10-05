2825ee4e9bb2acf067e9903277ce438dde52e597
Marius Brad <mbrad26@gmail.com>
2018-10-05 20:00:19 +0100

    ...

---
+++ pizzeria/p_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\mbrad\pycharmprojects\django\pizzeria\p_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
